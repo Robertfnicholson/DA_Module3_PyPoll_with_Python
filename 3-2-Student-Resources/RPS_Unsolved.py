d7c797c4ac8f44ec58d3ff6d6b0e207481e25b30
Robertfnicholson <93954896+Robertfnicholson@users.noreply.github.com>
2022-01-05 10:15:08 -0500

    Add files via upload

---
+++ 3-2-Student-Resources/RPS_Unsolved.py
@@ -0,0 +1,25 @@
+# Incorporate the random library
+import random
+
+# Print Title
+print("Let's Play Rock Paper Scissors!")
+
+# Specify the three options
+options = ["r", "p", "s"]
+
+# Computer Selection
+computer_choice = random.choice(options)
+
+# User Selection
+user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+
+# Run Conditionals
+If user_choice = "r"
+    computer_choice = "r"
+    print("tie. Start again")
+    LF computer_choice = "s"
+    Print("You win")
+    LF computer_choice = "p"
+    Print("Computer wins")
+    ' learn from this ' You need computer choice in conditional "...and computer_choice = "S""
+
